e2c08a49993a4e26b0957d1eefb342e91ba6cc86
PABLO RAFAEL CHAVONI ELY <80414553+PabloRafaelChavoniEly@users.noreply.github.com>
2022-04-06 13:23:39 -0400

    Add files via upload

---
+++ AULA 23-02-22/OlaMundo.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class OlaMundo {
+    public static void main(String[] args){
+    
+    // IMC
+       // Scanner entrada = new Scanner(System.in);
+
+       // System.out.println("Digite o seu peso:");
+       // float peso = entrada.nextFloat();
+       // System.out.println("Digite a sua altura: ");
+       // float altura = entrada.nextFloat();
+
+       // float imc = peso / (altura * altura);
+
+        // System.out.println("O seu IMC é: "+ imc);
+
+       // if (imc < 18.5) {
+       //     System.out.println("Slenderman");
+       // } else if (imc >= 18.5 && imc <= 24.9){
+       //     System.out.println("Humano");
+       // } else if (imc > 24.9 && imc <= 29.9){
+       //     System.out.println("Bolinha");
+       // } else if (imc > 29.9 && imc <= 34.9){
+       //     System.out.println("Pançudo");
+       // } else if (imc > 34.9 && imc <= 39.9){
+       //     System.out.println("Gordão");
+       // } else if (imc > 40){
+       //     System.out.println("Moby Dick");
+       // }
+
+    //Scanner
+        // Scanner entrada = new Scanner(System.in);
+
+        // System.out.println("Digite o seu nome:");
+        // String nome = entrada.nextLine();
+        // System.out.println("O seu nome é:" + nome);
+    
+    // Declaração de Variaveis
+        // int variavelInteira;
+        // String variavelString;
+        // TIPO       NOME
+
+        // variavelInteira = 10;
+        // variavelString = "Pablo";
+
+        // System.out.println(variavelInteira);
+        // System.out.println(variavelString);
+    
+    // Continha
+        //    int x = 10;
+        //    int y = 15;
+
+        //    System.out.println(x + y);
+        //    System.out.println(x * y);
+        //    System.out.println(x - y);
+        //    System.out.println(x / y);
+
+        // + somar
+        // - subtrair
+        // * multiplicar
+        // / dividir
+
+    // Nota
+        // int n1 = 9;
+        // int n2 = 8;
+        // int n3 = 9;
+    
+        // Float notaFinal = (float) (n1 + n2 + n3) / 3;
+        // String nota = "A nota da juliana é:";
+
+        // System.out.println(nota + notaFinal);
+    
+    // Concatenar
+        // int a = 1;
+        // int b = 2;
+
+        // System.out.println(a + b +", A soma de a + b é:" + (a + b));
+
+    //exercicio concluido
+        //digitar 3 notas, calcular a media, passou ou não passou, se passou beleza, se n passou faz exame ai, calcular a nota do exame, voce precisa tirar x para passar.
+
+    // Scanner entrada = new Scanner(System.in);
+
+    // System.out.println("Digite o seu nome: ");
+    // String nome = entrada.nextLine();
+    // System.out.println("Digite a primeira nota:");
+    // float n1 = entrada.nextFloat();
+    // System.out.println("Digite a segunda nota: ");
+    // float n2 = entrada.nextFloat();
+    // System.out.println("Digite a terceira nota: ");
+    // float n3 = entrada.nextFloat();
+
+    // float media = (n1 + n2 + n3) / 3;
+    // float exame = 10 - media;
+
+    // if (media >= 7){
+    //    System.out.println("Você passou, parabens!");
+    // }else if (media < 7){
+    //     System.out.println("Você não passou, terá que fazer o exame. Para passar você deve tirar " + exame + " no exame");
+    // }
+    }
+}
+
+
